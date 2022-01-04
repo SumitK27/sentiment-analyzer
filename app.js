f95f89363458b6c9b8d73e49c4b9ad2d9bd36333
SumitK27 <sumitkajbaje@gmail.com>
2022-01-04 17:38:58 +0530

    Removed Unnecessary code

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require("path");
 
 var PORT = process.env.PORT || 3000;
 
-var indexRouter = require("./routes/index");
 var nlpRouter = require("./routes/nlp");
 var analyzerRoute = require("./routes/analyzer");
 
@@ -13,9 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/api/nlp", nlpRouter);
 app.use("/api/analyzer", analyzerRoute);
+
+app.get("/", function (req, res) {
+    res.render("index.html");
+});
+
+app.get("/custom", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "index2.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server Listening on Port: ${PORT}`);
--- public/javascripts/index2.js
+++ public/javascripts/index2.js
@@ -11,26 +11,33 @@ const submitReview = (e) => {
     const title = document.getElementById("title");
     const outline = document.querySelector(":focus");
 
-    // Pre-trained Sentiment Analysis
-    fetch("/api/analyzer", options)
+    // Custom Sentiment Analysis
+    fetch("/api/nlp/s-analyzer", options)
         .then((res) => res.json())
-        .then(({ score }) => {
-            if (score >= 5) {
-                emojiSection.innerHTML = "😃";
-                title.style.color = "orange";
-                outline.style.borderColor = "orange";
-            } else if (score === 0) {
-                emojiSection.innerHTML = "😐";
-                title.style.color = "#00367c";
-                outline.style.borderColor = "#00367c";
-            } else if (score > 0) {
-                emojiSection.innerHTML = "🙂";
-                title.style.color = "green";
-                outline.style.borderColor = "green";
-            } else {
-                emojiSection.innerHTML = "😕";
+        .then(({ analysis, unfilteredAnalysis, analysis2, analysis3 }) => {
+            // Use average score
+            console.log("Unfiltered Score:", unfilteredAnalysis);
+            console.log("Analysis1 Score:", analysis);
+            console.log("Analysis2 Score:", analysis2.score);
+            console.log("Analysis3 Score:", analysis3.score);
+
+            if (analysis < 0) {
+                emojiSection.innerHTML =
+                    '<img src="https://img.icons8.com/office/80/000000/angry.png"/>';
                 title.style.color = "red";
                 outline.style.borderColor = "red";
+            }
+            if (analysis === 0) {
+                emojiSection.innerHTML =
+                    '<img src="https://img.icons8.com/officel/80/000000/neutral-emoticon.png">';
+                title.style.color = "#00367c";
+                outline.style.borderColor = "#00367c";
+            }
+            if (analysis > 0) {
+                emojiSection.innerHTML =
+                    '<img src="https://img.icons8.com/color/96/000000/happy.png">';
+                title.style.color = "green";
+                outline.style.borderColor = "green";
             }
         })
         .catch((err) => {
